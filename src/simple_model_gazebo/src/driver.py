801e8d7ebde01b31a0dffc7e9f3a0cb9946a9b3d
arismendyl <arismendyl@uninorte.edu.co>
2020-09-25 20:45:47 -0500

    pi

--- src/simple_model_gazebo/src/driver.py
+++ src/simple_model_gazebo/src/driver.py
@@ -6,10 +6,12 @@ from sensor_msgs.msg import Range
 from nav_msgs.msg import Odometry
 from tf.transformations import euler_from_quaternion, quaternion_from_euler
 import math
+import numpy as np
 
 idx = 0
 err_pre = 0
 err_acum = 0
+k3 = -0.00001
 
 def callback(sonar_c, sonar_r, sonar_l, odom):
     l_x = 0.0
@@ -18,16 +20,16 @@ def callback(sonar_c, sonar_r, sonar_l, odom):
     a_x = 0.0
     a_y = 0.0
     a_z = 0.0
-    if (sonar_c.range<=0.25 or sonar_r.range<=0.10 or sonar_l.range<=0.10):
+    if (sonar_c.range<=0.25 or sonar_r.range<=0.15 or sonar_l.range<=0.15):
         l_x = 0.0
         l_y = 0.0
         l_z = 0.0
         a_x = 0.0
         a_y = 0.0
         if sonar_r.range<sonar_l.range:
-            a_z = 0.3
+            a_z = 0.8
         else: 
-            a_z = -0.3
+            a_z = -0.8
     else:
         l_x, a_z = controller(odom)
         l_y = 0.0
@@ -39,29 +41,28 @@ def callback(sonar_c, sonar_r, sonar_l, odom):
     publisher(l_x,l_y,l_z,a_x,a_y,a_z)
 
 def controller(odom):
-    global idx,err_pre, err_acum
-    x_0 = [-1.0, 0.0]
-    y_0 = [0.0, 0.0]
+    global idx,err_pre, err_acum, k3
+    x_0 = [-3.5, 0.0]
+    y_0 = [4.0, 0.0]
     theta_0 = 0
     k1p = 0.25
-    k2d = 0.1
-    k1i = 0.5
     k2 = 0.9
     k3 = -0.3
-    #idx=0
     x = x_0[idx] + odom.pose.pose.position.x 
     y = y_0[idx] + odom.pose.pose.position.y
     quaternion = (odom.pose.pose.orientation.x, odom.pose.pose.orientation.y, odom.pose.pose.orientation.z, odom.pose.pose.orientation.w)
-    theta = euler_from_quaternion(quaternion)[2]
+    theta = euler_from_quaternion(quaternion)[2] + theta_0
     print("Odometry --> x: ",x,"y: ",y, "theta: ", theta)
     ed = math.sqrt((x*x) + (y*y))
     beta = -math.atan2(-y,-x)
     alfa = -beta - theta
+    if alfa < -np.pi:
+        alfa = alfa + 2*np.pi
+    elif alfa > np.pi:
+        alfa = alfa - 2*np.pi
     print("Error --> rho: ",ed,"alfa: ",alfa, "beta: ", beta)
     x_dot = k1p*ed  
-    t_dot = k2*alfa + k3*beta + k2d*(alfa - err_pre) + k2i*err_acum
-    err_pre = alfa
-    err_acum = err_acum + alfa
+    t_dot = k2*alfa + k3*beta 
     print("Control --> x_dot: ",x_dot,"theta_dot: ",t_dot)
     if ed < 0.03:
         idx = (idx + 1)%2
