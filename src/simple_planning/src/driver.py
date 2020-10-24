15c86b4369c096dc762d20cdbdda35748415f5a2
arismendyl <arismendyl@uninorte.edu.co>
2020-10-24 14:29:35 -0500

    python

--- src/simple_planning/src/driver.py
+++ src/simple_planning/src/driver.py
@@ -14,24 +14,24 @@ Y_0 = 1.95
 GRID_STEP = 0.3
 K1 = 0.7
 K2 = 0.7
-x_dest = 0
-y_dest = 0
 state = 0
 grid_x, grid_y, path = ([], ) * 3
-float ed, w, dx, dy, x_dot, t_dot
+x_dest, y_dest, x_dot, t_dot = (0.0, ) * 4
 
 def createMap():
-    global grid_x, grid_y
+    global grid_x, grid_y 
+    grid_x, grid_y = [0]*X_SIZE, [0]*Y_SIZE 
     for i in range(X_SIZE):
-        grid_x.append(X_0 + i*GRID_STEP)
+        grid_x[i] = X_0 + i*GRID_STEP
     for i in range(Y_SIZE):
-        grid_y.append(X_0 - i*GRID_STEP)
+        grid_y[i] = Y_0 - i*GRID_STEP
 
 def updateDest():
     global x_dest, y_dest
     node = input('Enter final node: ')
-    x = node % X_SIZE
-    y = (node - x) / X_SIZE
+    node = int(node)
+    x = int(node % X_SIZE)
+    y = int((node - x) / X_SIZE)
     x_dest = grid_x[y]
     y_dest = grid_y[x]
     print("Destination: x = ", x_dest, " y = ", y_dest)
@@ -39,31 +39,35 @@ def updateDest():
 def assignPath(paths):
     global path 
     path = paths
+    print(path.data)
 
 def odomCallback(odom):
-    global dx, dy, ed, w, x_dot, t_dot, state
-
+    global x_dot, t_dot, state
+    K1, K2 = 0.7, 0.7
+    pub = rospy.Publisher("/red_rider/cmd_vel", Twist, queue_size= 10)
+    msg = Twist()
+    msg.linear.x = x_dot
+    msg.angular.z = t_dot
+    pub.publish(msg)
     dx = x_dest - odom.pose.pose.position.x 
-    dy = y_dest + odom.pose.pose.position.y
+    dy = y_dest - odom.pose.pose.position.y
     ed = math.sqrt((dx*dx) + (dy*dy))
     quaternion = (odom.pose.pose.orientation.x, odom.pose.pose.orientation.y, odom.pose.pose.orientation.z, odom.pose.pose.orientation.w)
-    theta = euler_from_quaternion(quaternion)[2]
-    beta = -math.atan2(-dy,-dx)
-    w = -beta - theta
-    if (w > np.pi):
-        w -= 2*np.pi
-    elif (w < -np.pi):
-        w += 2*np.pi
+    w = math.atan2(dy,dx) - euler_from_quaternion(quaternion)[2]
+    if w < -np.pi:
+        w = w + 2*np.pi
+    elif w > np.pi:
+        w = w - 2*np.pi
     
     if (state == 0):
         x_dot = 0
-        t_dot = k2*w
-        if (abs(w) < 0.07):
+        t_dot = K2*w
+        if (math.fabs(w) < 0.07):
             state = 1
     elif (state == 1):
         if (ed > 0.07):
-            x_dot = k1*ed
-            t_dot = k2*w
+            x_dot = K1*ed
+            t_dot = K2*w
         else:
             x_dot = 0
             t_dot = 0
@@ -73,23 +77,16 @@ def odomCallback(odom):
         t_dot = 0
         updateDest()
         state = 0
+
     print("Error --> rho ", ed, ",alfa: ", w)
 
 if __name__ == '__main__':
     try:
+        rospy.init_node('rr_path_exec', anonymous=True)
         createMap()
         updateDest()
-        rospy.init_node('rr_path_exec', anonymous=True)
-        pub = rospy.Publisher("red_rider/cmd_vel", Twist, queue_size= 10)
         dest_v = rospy.Subscriber("next_position", Int32MultiArray, assignPath)
-        odom_sub = rospy.Subscriber("red_rider/odom", Int32MultiArray, odomCallback)
-        rate = rospy.Rate(30)
-        msg = Twist()
-        while not rospy.is_shutdown():
-            msg.linear.x = x_dot
-            msg.angular.z = t_dot
-            pub.publish(msg)
-            rospy.spin()
-            rate.sleep()
+        odom_sub = rospy.Subscriber("/red_rider/odom", Odometry, odomCallback)
+        rospy.spin()
     except rospy.ROSInterruptException:
         pass
